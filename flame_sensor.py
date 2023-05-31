d0fbdcbc2dbdc3cc6cbdf0fb8346544e655f4030
mdenisa06 <mihaela.mita@student.out.ro>
2023-05-31 19:58:16 +0300

    added firebase to raspberry code

--- flame_sensor.py
+++ flame_sensor.py
@@ -1,9 +1,21 @@
 import RPi.GPIO as GPIO
 import time
+import pyrebase
 
 GPIO.setmode(GPIO.BCM)
+
 RED_PIN = 17
 FLAME_PIN = 23
+
+config = {
+    "apiKey": "AIzaSyCWYL1nqsrug3kGGuigzE5TEJLSoO7mWgc",
+    "authDomain": "degi76-e7d8a.firebaseapp.com",
+    "databaseURL": "https://degi76-e7d8a-default-rtdb.firebaseio.com",
+    "storageBucket": "degi76-e7d8a.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 GPIO.setup(FLAME_PIN, GPIO.IN)
 GPIO.setup(RED_PIN, GPIO.OUT)
@@ -14,11 +26,15 @@ try:
 
 		if flame_state == GPIO.HIGH:
 			GPIO.output(RED_PIN, GPIO.HIGH)
-			print ("Fire, RUN!")
+			message = "Fire, RUN!"
+			print (message)
+			db.child("messages").push(message)
 			time.sleep(2)
 		else:
 			GPIO.output(RED_PIN, GPIO.LOW)
-			print("No fire, chill")
+			message="No fire, chill"
+			print(message)
+			db.child("messages").push(message)
 			time.sleep(2)
             
 except KeyboardInterrupt:
