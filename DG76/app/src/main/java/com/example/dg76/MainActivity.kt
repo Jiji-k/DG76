f59cdab3c706183d9487c1b0f14c62c005a5a68e
mdenisa06 <mihaela.mita@student.out.ro>
2023-06-01 15:15:40 +0300

    Updated Call and Messages Button

--- DG76/app/src/main/java/com/example/dg76/MainActivity.kt
+++ DG76/app/src/main/java/com/example/dg76/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.dg76
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.dg76.databinding.ActivityMainBinding
 
@@ -14,5 +18,33 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.mainViewModel = mainViewModel
         setContentView(binding.root)
+
+        CallForHelp()
+        MessageForHelp()
+    }
+
+    private fun CallForHelp() {
+        mainViewModel.onCallButtonClicked.observe(this) { value ->
+            if (value) {
+                val phoneNumber = "0751919106"
+                val dialIntent = Intent(Intent.ACTION_DIAL)
+                dialIntent.data = Uri.parse("tel:$phoneNumber")
+                startActivity(dialIntent)
+            }
+        }
+    }
+
+    private fun MessageForHelp() {
+        mainViewModel.onMessageButtonClicked.observe(this) { value ->
+            if (value) {
+                val phoneNumber = "0751919106"
+                val message = "My address is Strada Zorilor, nr 76, and my house is on fire. Need help!"
+
+                val smsUri = Uri.parse("smsto:$phoneNumber")
+                val sendIntent = Intent(Intent.ACTION_SENDTO, smsUri)
+                sendIntent.putExtra("sms_body", message)
+                startActivity(sendIntent)
+            }
+        }
     }
 }
--- DG76/app/src/main/java/com/example/dg76/MainViewModel.kt
+++ DG76/app/src/main/java/com/example/dg76/MainViewModel.kt
@@ -2,7 +2,16 @@ package com.example.dg76
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+    val onCallButtonClicked = MutableLiveData(false)
+    val onMessageButtonClicked = MutableLiveData(false)
+    fun onClickCallButton() {
+        onCallButtonClicked.value = true
+    }
 
+    fun onClickMessageButton() {
+        onMessageButtonClicked.value = true
+    }
 }
