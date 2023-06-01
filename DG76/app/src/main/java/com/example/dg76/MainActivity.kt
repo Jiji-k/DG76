e3c58896c527a961f73291ac74fb92af3b194113
mdenisa06 <mihaela.mita@student.out.ro>
2023-06-01 14:52:30 +0300

    Changed App Layout

--- DG76/app/src/main/java/com/example/dg76/MainActivity.kt
+++ DG76/app/src/main/java/com/example/dg76/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.dg76
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.example.dg76.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        setContentView(binding.root)
     }
 }
