8215c76cdfca0f4b47bf3e08017d2c14f8cc1fd7
mdenisa06 <mihaela.mita@student.out.ro>
2023-06-01 14:23:11 +0300

    Changed to MVVM

--- DG76/app/src/main/java/com/example/dg76/MainActivity.kt
+++ DG76/app/src/main/java/com/example/dg76/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.dg76
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
